36fed1b7054df676c4b2346812cf6725d500f115
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2022-05-01 18:38:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+nosex = 0
+nosey = 0
+
+function preload() {
+    clown_nose = loadImage('https: //i.postimg.cc/DwCwN10y/clown.jpg')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet is Intialized')
+}
+
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        nosex = results[0].pose.nose.x
+        nosey = results[0].pose.nose.y
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, nose_x, nose_y, 30, 30);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+}
+
+function take_snapshot() {
+    save('filteredimage.png');
+}
